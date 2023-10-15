bb64e7f47bfc61781af1fb34b9d1785c2988150e
aleksey <Kaloha215@gmail.com>
2023-10-15 21:13:47 +1000

    rename app-file and add template for user page

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 
 # Create a route decorator
 @app.route('/')
-# def index():
-#     return "<h1>Hello World!</h1>"
 def index():
     return render_template("index.html")
 
@@ -16,4 +14,4 @@ def index():
 # localhost:5000/user/Alex
 @app.route('/user/<name>')
 def user(name):
-    return "<h1>Hello {}</h1>".format(name)
+    return render_template("user.html", user_name=name)
